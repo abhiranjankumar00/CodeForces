fa2443942c434bca53ad1074095cb6d3a485f53d
Abhiranjan Kumar <abhiranjan.kumar00@gmail.com>
2013-10-03 08:32:32 +0530

    203

--- 203/E.cpp
+++ 203/E.cpp
@@ -1,98 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int inf = 1e5;
-
-int N, M, K;
-vector <int> a;
-vector <vector<int> > dist1, dist2;
+const int oo = 1e5;
 
 int main()
 {
+  int dist1[333][333], dist2[333][333];
+  int N, M, K;
+  vector <int> a;
+  vector <pair <int, int> > edges;
+
   cin >> N >> M >> K;
-  dist2  = vector <vector<int> > (N+1, vector <int> (N+1, inf));
-  dist2  = vector <vector<int> > (N+1, vector <int> (N+1, inf));
-
+  a.resize(K);
   for(int i = 0; i < (int)K; ++i) {
-    int u;
-    cin >> u;
-    a.push_back(u);
+    cin >> a[i];
   }
-
-  map <int, int> idx;
-  int k = 1;
-  for(int i = 0; i < (int)K; ++i) {
-    idx[a[i]]=k;
-    k++;
-  }
-  for(int i = 1; i <= N; i++)
-    if(idx.count(i) == 0)
-      idx[i] = k++;
-
 
   for(int i = (int)1; i <= (int)N; ++i) {
     for(int j = (int)1; j <= (int)N; ++j) {
-      if(i==j)
-        dist2[i][j] = dist2[i][j] = 0;
-      else
-        dist2[i][j] = dist2[i][j] = inf;
+      dist1[i][j] = dist2[i][j] = i==j?0:oo;
     }
   }
-
-  set<pair <int, int> > st;
-  vector <pair <int, int> > id;
-
-  for(int i = 1; i < (int)N-1; ++i) {
-    K--;
-    st.insert(make_pair(i,i+1));
+  for(int i = (int)1; i <= (int)N-1; ++i) {
+    dist1[i][i+1] = dist2[i][i+1] = 1;
+    dist1[i+1][i] = dist2[i+1][i] = 1;
+    M--;
   }
 
   for(int i = (int)1; i <= (int)N; ++i) {
-    for(int j = (int)i+1; j <= (int)N; ++j) {
-      if(st.find(make_pair(i, j)) != st.end())
-        id.push_back(make_pair(i, j));
+    for(int j = (int)i+2; j <= (int)N; ++j) {
+      edges.push_back(make_pair(i, j));
+    }
+ 
+  reverse(edges.begin(), edges.end());
+  for(int i = 0; i < (int)edges.size() && M > 0; ++i, M--) {
+    int u = edges[i].first, v = edges[i].second;
+    dist1[u][v] = dist1[v][u] = 1;
+    dist2[u][v] = dist2[v][u] = 1;
+  }
+
+  for(int i = (int)1; i <= (int)N; ++i) {
+    int v = i;
+    for(int j = (int)1; j <= (int)N; ++j) {
+      for(int r = (int)1; r <= (int)N; ++r) {
+        dist1[j][r] = min(dist1[j][r], dist1[j][v] + dist1[v][r]);
+      }
     }
   }
 
-  for(int i = id.size()-1; K>0 &&i >= 0; i--){
-    st.insert(id[i]);
-    K--;
-  }
-
-  for(int i = 0; i < (int)K; ++i) {
+  for(int i = (int)0; i <= (int)K-1; ++i) {
     int v = a[i];
     for(int j = (int)1; j <= (int)N; ++j) {
       for(int r = (int)1; r <= (int)N; ++r) {
-        dist2[j][r] =min(dist2[j][r], dist2[j][v]+dist2[v][r]);
+        dist2[j][r] = min(dist2[j][r], dist2[j][v] + dist2[v][r]);
       }
     }
   }
 
-  for(int i = 0; i < (int)N; ++i) {
-    int v = i+1;
-    for(int j = (int)1; j <= (int)N; ++j) {
-      for(int r = (int)1; r <= (int)N; ++r) {
-        dist2[j][r] =min(dist2[j][r], dist2[j][v]+dist2[v][r]);
-      }
-    }
-  }
   for(int i = (int)1; i <= (int)N; ++i) {
     for(int j = (int)1; j <= (int)N; ++j) {
       printf("%d ", dist1[i][j]);
     }
     cout << "\n";
   }
+  cout << "\n";
 
   for(int i = (int)1; i <= (int)N; ++i) {
     for(int j = (int)1; j <= (int)N; ++j) {
       printf("%d ", dist2[i][j]);
     }
-
     cout << "\n";
   }
-  if(dist1 == dist2){
-    printf("-1\n");
-  }
+  cout << "\n";
 
 	return 0;
 }
